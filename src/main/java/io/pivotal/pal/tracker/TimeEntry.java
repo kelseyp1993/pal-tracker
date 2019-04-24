96607ddeca6729c8093ff4058bfe555e3d756ba9
kelseyp1993 <kelsey.pritsker@gmail.com>
2019-04-24 15:16:44 -0700

    pal-tracker id name fix

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class TimeEntry {
-    private long timeEntryId;
+    private long id;
     private long projectId;
     private long userId;
     private LocalDate date;
@@ -18,7 +18,7 @@ public class TimeEntry {
     }
 
     public TimeEntry(long timeEntryId, long projectId, long userId, LocalDate parse, int hours) {
-        this.timeEntryId = timeEntryId;
+        this.id = timeEntryId;
         this.projectId = projectId;
         this.userId = userId;
         this.date = parse;
@@ -30,7 +30,7 @@ public class TimeEntry {
 
     public Long getId() {
 
-        return this.timeEntryId;
+        return this.id;
     }
 
     public long getProjectId() {
@@ -54,7 +54,7 @@ public class TimeEntry {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TimeEntry timeEntry = (TimeEntry) o;
-        return timeEntryId == timeEntry.timeEntryId &&
+        return id == timeEntry.id &&
                 projectId == timeEntry.projectId &&
                 userId == timeEntry.userId &&
                 hours == timeEntry.hours &&
@@ -63,6 +63,6 @@ public class TimeEntry {
 
     @Override
     public int hashCode() {
-        return Objects.hash(timeEntryId, projectId, userId, date, hours);
+        return Objects.hash(id, projectId, userId, date, hours);
     }
 }
